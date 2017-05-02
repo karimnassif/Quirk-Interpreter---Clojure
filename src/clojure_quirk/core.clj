10ff3dfe9b88db8b951457b969ce051527799fae
Karim Abou Nassif <karimabounassif@Davids-Air.interfacefinancial.com>
2017-05-02 13:00:09 -0400

    Several functions done, not runnable yet.

--- src/clojure_quirk/core.clj
+++ src/clojure_quirk/core.clj
@@ -2,17 +2,107 @@
   (:gen-class)
   (:require [instaparse.core :as insta]))
 
-;you can run this on the command line with: lein run -pt
+(defn third [alist] (nth alist 2))
+(defn fourth [alist] (nth alist 3))
+
+(defn ret-print [thingToPrint] 
+  (println thingToPrint)
+  thingToPrint
+)
+
+(defn CallByLabel [funLabel & args]
+  (apply(ns-resolve 'clojure-quirk.core (symbol(name funLabel))) args))
+
+
+;Program --> Statement Program | Program     
+(defn Program [subtree scope]   
+  (println "PROGRAM")
+  ;Program0
+  (cond (= 3 (count subtree))
+        ((CallByLabel (first (second subtree)) (second subtree) scope)
+        (CallByLabel (first (third subtree)) (third subtree) scope))
+   ;Program1
+   :else
+        (CallByLabel (first (second subtree)) (second subtree) scope)
+   )
+ )
+
+;Statement := FunctionDeclaration | Assignment | Print
+(defn Statement [subtree scope]
+  (println "STATEMENT")
+  (cond (= :FunctionDeclaration (first (second subtree)))
+              (CallByLabel (first (second subtree))(second subtree) scope)
+        (= :Assignment (first (second subtree)))
+              (CallByLabel (first (second subtree))(second subtree) scope)
+        (= :Print (first (second subtree)))
+              (CallByLabel (first (second subtree))(second subtree) scope))
+  )
+
+;Print := PRINT Expression
+(defn Print [subtree scope]
+  (println "PRINT")
+  (ret-print (CallByLabel (first (third subtree))(third subtree) scope))
+)
+        
+
+;Expression -->  Term ADD Expression | Term SUB Expression | Term    
+(defn Expression [subtree scope]
+  (println "EXPRESSION")
+  (cond (= 2 (count subtree))
+        (CallByLabel (first (second subtree))(second subtree) scope)
+        
+        (= :ADD (first (third subtree)))
+			          (+ (CallByLabel (first (second subtree))(second subtree) scope)
+			          (CallByLabel (first (fourth subtree))(fourth subtree) scope))
+		    (= :SUB (first (third subtree)))
+			           (- (CallByLabel (first (second subtree))(second subtree) scope)
+			           (CallByLabel (first (fourth subtree))(fourth subtree) scope))
+    )
+ ); end Expression
+
+;Term := Factor MULT Term | Factor DIV Term | Factor
+(defn Term [subtree scope] 
+  (println "TERM")
+  (cond (= 2 (count subtree))
+             (CallByLabel (first (second subtree))(second subtree) scope)
+        (= :MULT (first (third subtree)))
+             (* (CallByLabel (first (second subtree))(second subtree) scope)
+             (CallByLabel (first (fourth subtree))(fourth subtree) scope))
+        (= :DIV (first (third subtree)))
+             (/ (CallByLabel (first (second subtree))(second subtree) scope)
+             (CallByLabel (first (fourth subtree))(fourth subtree) scope)))
+   )
+
+;Factor := SubExpression EXP Factor | SubExpression | FunctionCall | Value EXP Factor | Value
+;              <*     ---------------------          *> TODO:FIX, fix a lot 
+(defn Factor [subtree scope] 
+  (println "FACTOR")
+  (cond (= 3 (count subtree))
+             (println "Gotta do exp")
+             ;(^ (CallByLabel (first (second subtree))(second subtree) scope)
+             ;(CallByLabel (first (fourth subtree))(fourth subtree) scope))
+        (= :FunctionCall (first (second subtree))
+             (CallByLabel (first (second subtree))(second subtree) scope))
+        (= :Value (first (second subtree))
+             (CallByLabel (first (second subtree))(second subtree) scope))
+        )
+  )
+        
+        
+(defn interpret-quirk [subtree scope] 
+  (println "Interpreting")
+  (CallByLabel (first subtree) subtree {} ))
+
 (defn -main [& args]
- ;(println (first *command-line-args*))
  (if (.equals "-pt" (first *command-line-args*))
    (def SHOW_PARSE_TREE true)
  )
  (def quirk-parser (insta/parser (slurp "resources/quirk-grammar-ebnf") :auto-whitespace :standard))
- (def parse-tree (quirk-parser "function foo (x, y) { return 3+4 +2 / x ^ y} print foo(12, 12)"))
+ ;(def parse-tree (quirk-parser "function foo(x){return x+5} print foo"))
+ (def parse-tree (quirk-parser "print 5"))
  (if(= true SHOW_PARSE_TREE)
-       (println parse-tree)
+       ;(println parse-tree)
+       ;(println (count parse-tree))
+       (interpret-quirk parse-tree {})
 )
 )
-
-(-main)
